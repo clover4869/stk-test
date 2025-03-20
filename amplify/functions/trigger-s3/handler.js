3d100849b01a11ee52f8c9e87f4bb4c10dcaaa21
clover4869 <153183368+clover4869@users.noreply.github.com>
2025-03-20 08:49:41 +0000

    upload

--- amplify/functions/trigger-s3/handler.js
+++ amplify/functions/trigger-s3/handler.js
@@ -1,13 +1,65 @@
-module.exports.handler = async (event) => {
-    console.log('======================================================================================');
-    console.log('Received SQS event:', JSON.stringify(event, null, 2));
-    console.log( process.env.TEST_ENV, {process} );
-    for (const record of event.Records) {
-        console.log('Processing SQS message:', record.body);
-    }
-    console.log('======================================================================================');
-    return {
-        statusCode: 200,
-        body: 'Messages processed successfully!',
+const AWS = require("aws-sdk");
+const dotenv = require("dotenv");
+
+// Load environment variables from `.env` only in local development
+if (process.env.CURRENT_ENV === "local") {
+  dotenv.config();
+}
+
+const sqs = new AWS.SQS();
+const queueUrl = process.env.QUEUE_URL || "";
+
+exports.handler = function (event, context, callback) {
+  console.log("Received event:", JSON.stringify(event, null, 2));
+  console.log("Environment:", process.env.CURRENT_ENV);
+
+  try {
+    const record = event.Records[0];
+    const bucketName = record.s3.bucket.name;
+    const objectKey = record.s3.object.key;
+    const s3Path = "s3://" + bucketName + "/" + objectKey;
+
+    const message = {
+      bucket: bucketName,
+      object_key: objectKey,
+      s3_path: s3Path,
     };
+
+    const params = {
+      QueueUrl: queueUrl,
+      MessageBody: JSON.stringify(message),
+    };
+    
+    console.log({message, params});
+
+    sqs.sendMessage(params, function (err, data) {
+      if (err) {
+        console.error("Error sending message:", err);
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify({
+            message: "Error sending message",
+            error: err.message,
+          }),
+        });
+      } else {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: "Message sent successfully",
+            messageId: data.MessageId,
+          }),
+        });
+      }
+    });
+  } catch (error) {
+    console.error("Error processing event:", error);
+    callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Error processing event",
+        error: error.message,
+      }),
+    });
+  }
 };
--- amplify/functions/trigger-s3/resource.ts
+++ amplify/functions/trigger-s3/resource.ts
@@ -2,37 +2,39 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3Notifications from 'aws-cdk-lib/aws-s3-notifications';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 import { CfnOutput } from 'aws-cdk-lib';
 
 export class S3TriggerStkUploadStack extends cdk.Stack {
-  public readonly fileUploadBucket: s3.IBucket; 
-
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, {
       ...props
     });
 
-    this.fileUploadBucket = s3.Bucket.fromBucketName(this, 'StkUploadFile', 'stk-upload-file');
-    console.log({process, 'process.env.TEST_ENV' : process.env.TEST_ENV, 'process.env.TEST_ENV_BUILD' : process.env.TEST_ENV_BUILD})
+    // define bucket
+    const bucket = s3.Bucket.fromBucketName(this, 'StkUploadFile', 'dev-stk-original-s3');
+    
+    // Create SQS queue
+    const queue = new sqs.Queue(this, 'dev-s3-upload-queue', {
+      visibilityTimeout: cdk.Duration.seconds(30),
+    });
 
     const s3TriggerLambda = new lambda.Function(this, 'S3TriggerLambda', {
-      runtime: lambda.Runtime.NODEJS_18_X, // Specify the runtime
-      handler: 'handler.handler',           // Specify the handler function
+      runtime: lambda.Runtime.NODEJS_18_X, 
+      handler: 'handler.handler',
       code: lambda.Code.fromAsset('./amplify/functions/trigger-s3'),
       functionName: 'S3TriggerLambda',
-      description: 'This is my custom Lambda function created using CDK',
+      description: 'Custom Lambda function created using CDK. Using to trigger upload file from s3 and push msg to sqs.',
       memorySize: 128,
       environment: {
-        TEST: process.env.TEST_ENV || 'process.env.TEST_ENV doesnot have value',
-        ENVIRONMENT: process.env.ENVIRONMENT   || 'process.env.ENVIRONMENT ',
-        LOCAL : process.env.LOCAL   || 'process.env.LOCAL '
+        QUEUE_URL: queue.queueUrl,
       }
     });
 
-    this.fileUploadBucket.grantRead(s3TriggerLambda);
+    bucket.grantRead(s3TriggerLambda);
 
-    this.fileUploadBucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3Notifications.LambdaDestination(s3TriggerLambda));
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3Notifications.LambdaDestination(s3TriggerLambda));
 
     new CfnOutput(this, 'S3TriggerLambdaArn', {
       value: s3TriggerLambda.functionArn,
