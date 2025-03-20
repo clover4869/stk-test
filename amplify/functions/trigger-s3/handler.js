52ce49913d23fca98c11d8f24fa7d56421c794a6
clover4869 <153183368+clover4869@users.noreply.github.com>
2025-03-20 03:30:48 +0000

    test: env

--- amplify/functions/trigger-s3/handler.js
+++ amplify/functions/trigger-s3/handler.js
@@ -1,10 +1,11 @@
 module.exports.handler = async (event) => {
+    console.log('======================================================================================');
     console.log('Received SQS event:', JSON.stringify(event, null, 2));
-
+    console.log( process.env.TEST_ENV );
     for (const record of event.Records) {
         console.log('Processing SQS message:', record.body);
     }
-
+    console.log('======================================================================================');
     return {
         statusCode: 200,
         body: 'Messages processed successfully!',
